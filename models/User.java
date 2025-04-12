b4d762ef79e0a07d873024fcbc890aa58bc1a895
YameeOhira <kaibaoroblox@gmail.com>
2025-04-13 03:39:55 +0800

    added Name for User and all get / set methods

--- models/User.java
+++ models/User.java
@@ -4,10 +4,44 @@ import models.enumerations.*;
 public class User {
 
 	private String NRIC;
+	private String Name;
 	private String password;
 	private int age;
 	private MaritalState maritalStatus;
 	private String filterSettings;
 	private Role role;
 
+    // Getter methods
+    public String getNRIC() {
+        return NRIC;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public MaritalState getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public String getFilterSettings() {
+        return filterSettings;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    // Setter method for password only
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
