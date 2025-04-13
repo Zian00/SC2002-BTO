364d4f20adca303200d6fd270abc389b82cb4851
YameeOhira <kaibaoroblox@gmail.com>
2025-04-13 18:38:00 +0800

    Add initial implementations for view and controller classes: OfficerApplicationCTRL, BTOApplicationView, BTOProjectView, EnquiryView, and OfficerApplicationView with placeholder methods.

---
+++ views/OfficerApplicationView.java
@@ -0,0 +1,55 @@
+package views;
+import java.util.List;
+import models.BTOProject;
+import models.OfficerApplication;
+import models.enumerations.RegistrationStatus;
+
+public class OfficerApplicationView {
+
+	public void displayMenu() {
+		// TODO - implement OfficerApplicationView.displayMenu
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param officerApplicaitonList
+	 */
+	public void displayAllOfficerApplications(List<OfficerApplication> officerApplicaitonList) {
+		// TODO - implement OfficerApplicationView.displayAllOfficerApplications
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param application
+	 */
+	public void approveOfficerApplication(OfficerApplication application) {
+		// TODO - implement OfficerApplicationView.approveOfficerApplication
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param application
+	 */
+	public void rejectOfficerApplication(OfficerApplication application) {
+		// TODO - implement OfficerApplicationView.rejectOfficerApplication
+		throw new UnsupportedOperationException();
+	}
+
+	public RegistrationStatus displayRegistrationStatus() {
+		// TODO - implement OfficerApplicationView.displayRegistrationStatus
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param project
+	 */
+	public void registerForProjectOfficer(BTOProject project) {
+		// TODO - implement OfficerApplicationView.registerForProjectOfficer
+		throw new UnsupportedOperationException();
+	}
+
+}
